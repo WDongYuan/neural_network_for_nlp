e999a0372d456e2553356df39a517d28cfbcd2e8
WeidongYuan <weidongy@andrew.cmu.edu>
2017-10-15 23:05:13 -0400

    debug

--- baseline_model/GPU_RunModel_batch.py
+++ baseline_model/GPU_RunModel_batch.py
@@ -60,7 +60,7 @@ def Accuracy(model,data):
 
 def TwoPointerAccuracy(model,data):
 	random.shuffle(data)
-	data = data[0:1000]
+	data = data[0:500]
 	batch_size = len(data)
 
 	batch_context = [sample.context_token for sample in data]
